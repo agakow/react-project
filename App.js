c1640d1b0607cd48330b1d8240ed895853a93fed
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-09-06 13:52:56 +0100

    setup

---
+++ App.js
@@ -0,0 +1,15 @@
+// jshint ignore: start
+
+import React from 'react';
+class App extends React.Component {
+  render(){
+    return <div>Hello</div>
+  }
+}
+
+const App = () => <h1>Hello</h1>
+
+export default App;
+
+//class Component has state
+//constant Component does not have state
