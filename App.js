99fef7446c4999cd0c9121d9ebb3ab690c9a4180
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-09-06 15:12:50 +0100

    mounting usage

--- App.js
+++ App.js
@@ -12,17 +12,21 @@ class App extends React.Component {
     this.setState({val: this.state.val + 1})
   }
   componentWillMount(){
-    console.log('mounting!');
+    this.setState({multiplier: 2})
   }
   render() {
     console.log('rendering!');
-    return <button onClick={this.update}>{this.state.val}</button>
+    return (
+      <button onClick={this.update}>
+        {this.state.val * this.state.multiplier}
+      </button>
+    );
   }
   componentDidMount(){
-    console.log('mounted!');
+    this.increment = setInterval(this.update, 500)
   }
   componentWillUnmount(){
-    console.log('bye!');
+    clearInterval(this.increment)
   }
 }
 
