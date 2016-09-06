286e9bd8349341f0af9c01ce556b536a760c3dfc
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-09-06 15:06:14 +0100

    componentlifecycle - mounting basics

--- App.js
+++ App.js
@@ -5,63 +5,52 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      red: 0,
-      green: 0,
-      blue: 0
-    }
+    this.state = {val: 0};
     this.update = this.update.bind(this)
+    }
+  update(){
+    this.setState({val: this.state.val + 1})
   }
-  update(e){
-    this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
-      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
-      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
-    })
+  componentWillMount(){
+    console.log('mounting!');
   }
-  render(){
-    return (
-      <div>
-        <Slider ref="red" update={this.update} />
-        {this.state.red}
-        <Slider ref="green" update={this.update} />
-        {this.state.green}
-        <Slider ref="blue" update={this.update} />
-        {this.state.blue}
-        <br/>
-        <br/>
-        <Button>I <Heart/> React</Button>
-      </div>
-    );
-  }
-}
-
-class Button extends React.Component {
   render() {
-    return <button> {this.props.children} </button>
+    console.log('rendering!');
+    return <button onClick={this.update}>{this.state.val}</button>
+  }
+  componentDidMount(){
+    console.log('mounted!');
+  }
+  componentWillUnmount(){
+    console.log('bye!');
   }
 }
 
-const Heart = () => <span className="glyphicon glyphicon-heart"></span>
-
-class Slider extends React.Component {
+class Wrapper extends React.Component {
+  constructor() {
+    super();
+  }
+  mount() {
+    ReactDOM.render(<App />, document.getElementById('a'))
+  }
+  unmount() {
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+  }
   render() {
     return (
       <div>
-      <input ref="inp" type="range"
-        min="0"
-        max="255"
-        onChange={this.props.update} />
+        <button onClick={this.mount.bind(this)}>Mount</button>
+        <button onClick={this.unmount.bind(this)}>Unmount</button>
+        <div id="a"></div>
       </div>
-    );
+    )
   }
 }
-
-
-
 ReactDOM.render(
-  <App />, document.getElementById('app')
+  <Wrapper />, document.getElementById('app')
 );
+
+
 
 //class Component has state
 //constant Component does not have state
@@ -71,3 +60,58 @@ ReactDOM.render(
 //refs are a way to reference an instance of a component from within the react app
 //refs wont work with stateless components (const)
 //access child nodes through this.props.children
+
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       red: 0,
+//       green: 0,
+//       blue: 0
+//     }
+//     this.update = this.update.bind(this)
+//   }
+//   update(e){
+//     this.setState({
+//       red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+//       green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+//       blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+//     })
+//   }
+//   render(){
+//     return (
+//       <div>
+//         <Slider ref="red" update={this.update} />
+//         {this.state.red}
+//         <Slider ref="green" update={this.update} />
+//         {this.state.green}
+//         <Slider ref="blue" update={this.update} />
+//         {this.state.blue}
+//         <br/>
+//         <br/>
+//         <Button>I <Heart/> React</Button>
+//       </div>
+//     );
+//   }
+// }
+//
+// class Button extends React.Component {
+//   render() {
+//     return <button> {this.props.children} </button>
+//   }
+// }
+//
+// const Heart = () => <span className="glyphicon glyphicon-heart"></span>
+//
+// class Slider extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//       <input ref="inp" type="range"
+//         min="0"
+//         max="255"
+//         onChange={this.props.update} />
+//       </div>
+//     );
+//   }
+// }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # React excercise
+https://egghead.io/lessons/react-fundamentals
